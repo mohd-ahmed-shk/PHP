a3976f3c1a52a13eed2add8a9de2f419dc8cb728
Ahmed Sheikh <97376286+unknown-sheikh@users.noreply.github.com>
2023-01-23 23:57:06 +0530

    Add files via upload

---
+++ web/about.php
@@ -0,0 +1,19 @@
+<?php
+    $con = mysqli_connect('localhost','root');
+    if($con) {
+        echo "success";
+    } else {
+        echo "failed";
+    }
+
+    mysqli_select_db($con,'photography');
+
+    $name  = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+
+    $query = "INSERT INTO users (name,email,number) VALUES ('$name','$email','$number')";
+
+    mysqli_query($con,$query);
+    header('location:index.php');
+?>
